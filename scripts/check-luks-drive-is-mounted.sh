ea7e7fc6bea3c4924405e5f846fc0e06ad0e69b8
Kuba86 <iqvr35my@duck.com>
2023-07-26 20:53:45 +0000

    draft of syncthing and mount check

---
+++ scripts/check-luks-drive-is-mounted.sh
@@ -0,0 +1,5 @@
+check if /mnt/[drive-name] is available.
+if yes then
+  stop
+if no then
+  send to matrix room
---
+++ scripts/check-syncthing-running.sh
@@ -0,0 +1,5 @@
+check if syncthing unit file is running.
+if yes then
+  stop
+if no then
+  get last 20 lines of logs and send to matrix room
